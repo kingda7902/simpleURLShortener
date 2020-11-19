0a41d8a477b0d099083b0c86c4f5888b9b5f98e7
Jason Peng <mijasoda@gmail.com>
2020-11-19 14:57:02 +0800

    Add gunicorn and redis for multiprocess and cache

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,4 @@ COPY requirements.txt ./
 RUN pip3 install -r requirements.txt
 COPY ./ ./app
 
-ENV FLASK_APP app/app.py
-
-CMD python3 -m flask run
+CMD gunicorn --reload --bind 0.0.0.0:8000 app.app:app
--- app.py
+++ app.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return 'Index Page'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
 
 
 if __name__ == '__main__':
