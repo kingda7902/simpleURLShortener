3d634d8f6515d3137090eab105a8d67e709d69be
Jason Peng <mijasoda@gmail.com>
2020-11-30 16:37:15 +0800

    Add template and gunicorn

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def index():
-    return 'Index Page'
+@app.route('/<redirect_id>')
+def index(redirect_id=None):
+    if redirect_id:
+        return redirect('https://www.google.com')
+    return render_template('shortener.html')
 
 
-@app.route('/post/<int:post_id>')
+@app.route('/post/<post_id>')
 def show_post(post_id):
     # show the post with the given id, the id is an integer
     return 'Post %d' % post_id
 
 
+@app.route('/shortify', methods=['POST'])
+def shortify():
+    return {'short_link': 'test'}
+
+
 if __name__ == '__main__':
+    app.debug = True
     app.run()
---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+bind = "0.0.0.0:8000"
+workers = os.cpu_count() * 2
